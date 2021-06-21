93f73031a698872059b852138df693e50ecb2bd6
willipedsu1 <85900225+willipedsu1@users.noreply.github.com>
2021-06-21 15:27:12 -0500

    Commit en Git Hub

--- src/Saludo.java
+++ src/Saludo.java
@@ -2,7 +2,6 @@ public class Saludo {
 
     public static void main (String[] args){
         System.out.println("Hola Mundo");
-        System.out.println("Actualizacion");
         System.out.println("Actualizacion Local");
     }
 
